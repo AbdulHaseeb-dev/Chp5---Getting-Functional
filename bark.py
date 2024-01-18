02d7ac2461531bef46341428b5db88525ac0e353
AbdulHaseeb-dev <haseebrana.faisal@gmail.com>
2024-01-18 20:37:27 +0500

    built a function

---
+++ bark.py
@@ -0,0 +1,11 @@
+def bark(name, wheight):
+    if wheight > 20 :
+        print(name, " says WOOF ,WOOF")
+    else :
+        print(name, "says woof, woof") 
+
+bark('Codie', 40)
+bark('Sparky', 9)
+bark('Jackson', 12)
+bark('Fido', 65)
+    
